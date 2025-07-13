e9d8a69c305dbdbaf9fae373c5c09552f70e92fe
Szymon Piątek <szymon.piatek125@gmail.com>
2025-07-13 21:32:45 +0200

    DPF cleaning

--- classes/form_builder/dpf/screenplay_scholarship/application_builder.py
+++ classes/form_builder/dpf/screenplay_scholarship/application_builder.py
@@ -10,9 +10,162 @@ class ScreenplayScholarshipApplicationBuilder(DPFApplicationBuilder):
 
         self.priority_data_path = self.department_data_path / 'screenplay_scholarship'
 
-    def create_application_information_data(self):
-        part = self.load_json(path=self.priority_data_path / '_pages' / 'application_information_data.json')
-        self.save_part(part=part)
+    def create_application_metadata(self, task_type: str = 'Stypendium scenariuszowe'):
+        DPFApplicationBuilder.create_application_metadata(self, task_type)
+
+    def create_application_basic_data(self, **kwargs):
+        section_path = self.department_data_path / '_pages' / 'application_basic_data'
+
+        part = self.create_part(
+            title='I. Dane podstawowe',
+            short_name='I. Dane podstawowe',
+        )
+
+        sections = [
+            {
+                "path": section_path / "scope_of_project.json",
+                "data": {
+                    "number": "1",
+                    "scopeOfProject": {
+                        "options": [
+                            "Stypendium scenariuszowe"
+                        ]
+                    }
+                }
+            },
+            {
+                "path": section_path / "movie_kind.json",
+                "data": {
+                    "number": "2",
+                    "movieKind": {
+                        "options": [
+                            "fabularny",
+                            "dokumentalny",
+                            "animowany"
+                        ]
+                    },
+                    "typeOfProject": {
+                        "options": [
+                            "stypendium scenariuszowe"
+                        ],
+                        "validators": [
+                            {
+                                "name": "RequiredValidator"
+                            },
+                        ],
+                        "calculationRules": []
+                    }
+                }
+            },
+            {
+                "path": section_path / "movie_subject.json",
+                "data": {
+                    "number": "3",
+                    "movieSubject": {
+                        "options": [
+                            "film autorski",
+                            "film o tematyce historycznej",
+                            "film dla młodego widza i widowni familijnej"
+                        ],
+                        "validators": [
+                            {
+                                "name": "RelatedAllowedOptionsValidator",
+                                "kwargs": {
+                                    "field_name": "movieKind",
+                                    "mapping": {
+                                        "fabularny": [
+                                            "film autorski",
+                                            "film o tematyce historycznej",
+                                            "film dla młodego widza i widowni familijnej"
+                                        ],
+                                        "animowany": [
+                                            "film autorski",
+                                            "film o tematyce historycznej",
+                                            "film dla młodego widza i widowni familijnej"
+                                        ],
+                                        "dokumentalny": [
+                                            "film autorski",
+                                            "film o tematyce historycznej"
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                "name": "RequiredValidator"
+                            }
+                        ]
+                    }
+                }
+            },
+            {
+                "path": section_path / "piece_title.json",
+                "data": {
+                    "number": "4"
+                }
+            },
+            {
+                "path": section_path / "short_movie_description.json",
+                "data": {
+                    "number": "5"
+                }
+            },
+            {
+                "path": section_path / "application_relates.json",
+                "data": {
+                    "number": "6",
+                    "applicationRelates": {
+                        "options": [
+                            "umowa"
+                        ]
+                    }
+                }
+            },
+            {
+                "path": section_path / "type_of_support.json",
+                "data": {
+                    "number": "7",
+                    "typeOfSupport": {
+                        "options": [
+                            "stypendium scenariuszowe"
+                        ]
+                    }
+                }
+            }
+        ]
+
+        self.create_part_by_sections(part=part, sections=sections)
+
+    def create_application_applicant_data(self, **kwargs):
+        part = self.create_part(
+            title="II. Dane wnioskodawcy",
+            short_name="II. Dane wnioskodawcy"
+        )
+
+        sections = [
+            {
+                "path": self.department_data_path / '_pages' / 'application_applicant_data' / 'applicant_full_name.json',
+                "data": {
+                    "number": "1"
+                }
+            },
+            {
+                "path": self.priority_data_path / '_pages' / 'application_applicant_data' / 'applicant_data.json',
+                "data": {
+                    "number": "2"
+                }
+            },
+            {
+                "path": self.application_data_path / '_pages' / 'application_applicant_data' / 'responsible_person.json',
+                "data": {
+                    "number": "3"
+                }
+            }
+        ]
+
+        self.create_part_by_sections(
+            part=part,
+            sections=sections
+        )
 
     def create_application_completion_date_data(self, **kwargs):
         priority_data_path = self.priority_data_path / '_pages' / 'application_completion_date_data'
@@ -61,182 +214,3 @@ class ScreenplayScholarshipApplicationBuilder(DPFApplicationBuilder):
             ]
         )
         self.save_part(part)
-
-    def create_application_additional_data(self):
-        part = self.load_json(path=self.priority_data_path / '_pages' / 'application_additional_data.json')
-        self.save_part(part=part)
-
-    def create_application_attachments(self):
-        part = self.load_json(path=self.priority_data_path / '_pages' / 'application_attachments.json')
-        self.save_part(part=part)
-
-    def create_application_statements(self):
-        part = self.load_json(path=self.priority_data_path / '_pages' / 'application_statements.json')
-        self.save_part(part=part)
-
-    def generate(self):
-        self.create_application_base()
-
-        # Metadane wniosku
-        self.create_application_metadata(task_type='Stypendium scenariuszowe')
-
-        # I. Dane podstawowe
-        section_path = self.department_data_path / '_pages' / 'application_basic_data'
-
-        self.create_application_basic_data(
-            sections=[
-                {
-                    "path": section_path / "scope_of_project.json",
-                    "data": {
-                        "number": "1",
-                        "scopeOfProject": {
-                            "options": [
-                                "Stypendium scenariuszowe"
-                            ]
-                        }
-                    }
-                },
-                {
-                    "path": section_path / "movie_kind.json",
-                    "data": {
-                        "number": "2",
-                        "movieKind": {
-                            "options": [
-                                "fabularny",
-                                "dokumentalny",
-                                "animowany"
-                            ]
-                        },
-                        "typeOfProject": {
-                            "options": [
-                                "stypendium scenariuszowe"
-                            ],
-                            "validators": [
-                                {
-                                    "name": "RequiredValidator"
-                                },
-                            ],
-                            "calculationRules": []
-                        }
-                    }
-                },
-                {
-                    "path": section_path / "movie_subject.json",
-                    "data": {
-                        "number": "3",
-                        "movieSubject": {
-                            "options": [
-                                "film autorski",
-                                "film o tematyce historycznej",
-                                "film dla młodego widza i widowni familijnej"
-                            ],
-                            "validators": [
-                                {
-                                    "name": "RelatedAllowedOptionsValidator",
-                                    "kwargs": {
-                                        "field_name": "movieKind",
-                                        "mapping": {
-                                            "fabularny": [
-                                                "film autorski",
-                                                "film o tematyce historycznej",
-                                                "film dla młodego widza i widowni familijnej"
-                                            ],
-                                            "animowany": [
-                                                "film autorski",
-                                                "film o tematyce historycznej",
-                                                "film dla młodego widza i widowni familijnej"
-                                            ],
-                                            "dokumentalny": [
-                                                "film autorski",
-                                                "film o tematyce historycznej"
-                                            ]
-                                        }
-                                    }
-                                },
-                                {
-                                    "name": "RequiredValidator"
-                                }
-                            ]
-                        }
-                    }
-                },
-                {
-                    "path": section_path / "piece_title.json",
-                    "data": {
-                        "number": "4"
-                    }
-                },
-                {
-                    "path": section_path / "short_movie_description.json",
-                    "data": {
-                        "number": "5"
-                    }
-                },
-                {
-                    "path": section_path / "application_relates.json",
-                    "data": {
-                        "number": "6",
-                        "applicationRelates": {
-                            "options": [
-                                "umowa"
-                            ]
-                        }
-                    }
-                },
-                {
-                    "path": section_path / "type_of_support.json",
-                    "data": {
-                        "number": "7",
-                        "typeOfSupport": {
-                            "options": [
-                                "stypendium scenariuszowe"
-                            ]
-                        }
-                    }
-                }
-            ]
-        )
-
-        # II. Dane wnioskodawcy
-        self.create_application_applicant_data(
-            sections=[
-                {
-                    "path": self.department_data_path / '_pages' / 'application_applicant_data' / 'applicant_full_name.json',
-                    "data": {
-                        "number": "1"
-                    }
-                },
-                {
-                    "path": self.priority_data_path / '_pages' / 'application_applicant_data' / 'applicant_data.json',
-                    "data": {
-                        "number": "2"
-                    }
-                },
-                {
-                    "path": self.application_data_path / '_pages' / 'application_applicant_data' / 'responsible_person.json',
-                    "data": {
-                        "number": "3"
-                    }
-                }
-            ]
-        )
-
-        # III. Informacje
-        self.create_application_information_data()
-
-        # IV. Termin realizacji
-        self.create_application_completion_date_data()
-
-        # V. Dane finansowe
-        self.create_application_financial_data()
-
-        # VI. Dane dodatkowe
-        self.create_application_additional_data()
-
-        # VII. Załączniki
-        self.create_application_attachments()
-
-        # VIII. Oświadczenia
-        self.create_application_statements()
-
-        self.save_output()
