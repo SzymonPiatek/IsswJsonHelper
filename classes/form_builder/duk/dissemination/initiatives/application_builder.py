eeb5f2da9c4d660bba5e14097c9d78cb67d2cb6e
Szymon Piątek <szymon.piatek125@gmail.com>
2025-08-27 12:01:05 +0200

    Duk disseminatino project cost

--- classes/form_builder/duk/dissemination/initiatives/application_builder.py
+++ classes/form_builder/duk/dissemination/initiatives/application_builder.py
@@ -1,5 +1,7 @@
 from classes.form_builder.duk.dissemination.application_builder import DisseminationApplicationBuilder
 from classes.form_builder.duk.application_builder import DUKApplicationBuilder
+from classes.form_builder.estimate_builder.duk.application_estimate_builder import DUKApplicationEstimateBuilder
+from classes.form_builder.duk.dissemination.initiatives.estimate_data import estimate_sections_pt1, estimate_sections_pt2, estimate_sections_pt3, estimate_sections_pt4
 
 
 class InitiativesApplicationBuilder(DisseminationApplicationBuilder):
@@ -26,3 +28,91 @@ class InitiativesApplicationBuilder(DisseminationApplicationBuilder):
             }
         }
         DUKApplicationBuilder.create_application_basic_data(self=self, data=data)
+
+    def create_application_project_costs(self):
+        estimate_pt1 = DUKApplicationEstimateBuilder(
+            estimate_sections=estimate_sections_pt1,
+            after_name="Pt1"
+        )
+        estimate_pt2 = DUKApplicationEstimateBuilder(
+            estimate_sections=estimate_sections_pt2,
+            after_name="Pt2"
+        )
+        estimate_pt3 = DUKApplicationEstimateBuilder(
+            estimate_sections=estimate_sections_pt3,
+            after_name="Pt3"
+        )
+        estimate_pt4 = DUKApplicationEstimateBuilder(
+            estimate_sections=estimate_sections_pt4,
+            after_name="Pt4"
+        )
+
+        part = self.create_part(
+            title="VII. Kosztorys przedsięwzięcia",
+            short_name="VII. Kosztorys przedsięwzięcia",
+            chapters=[
+                estimate_pt1.generate_estimate_top(),
+                estimate_pt1.generate_estimate_headers(),
+                self.create_chapter(
+                    components=[
+                        self.create_chapter(
+                            visibility_rules=[
+                                self.visibility_rule.depends_on_value(
+                                    field_name="projectType",
+                                    values=[
+                                        "Organizacja przeglądów i innych wydarzeń filmowych o charakterze lokalnym"
+                                    ]
+                                )
+                            ],
+                            components=[
+                                estimate_pt1.generate_estimate(),
+                            ]
+                        ),
+                        self.create_chapter(
+                            visibility_rules=[
+                                self.visibility_rule.depends_on_value(
+                                    field_name="projectType",
+                                    values=[
+                                        "Działalność kin studyjnych i lokalnych"
+                                    ]
+                                )
+                            ],
+                            components=[
+                                estimate_pt2.generate_estimate(),
+                            ]
+                        ),
+                        self.create_chapter(
+                            visibility_rules=[
+                                self.visibility_rule.depends_on_value(
+                                    field_name="projectType",
+                                    values=[
+                                        "Działalność klubów filmowych"
+                                    ]
+                                )
+                            ],
+                            components=[
+                                estimate_pt3.generate_estimate(),
+                            ]
+                        ),
+                        self.create_chapter(
+                            visibility_rules=[
+                                self.visibility_rule.depends_on_value(
+                                    field_name="projectType",
+                                    values=[
+                                        "Organizacja kongresów, konferencji i sympozjów, mających na celu pogłębianie i upowszechnianie wiedzy o filmie",
+                                        "Organizacja konkursów filmowych, wystaw oraz innych wydarzeń promujących sztukę filmową",
+                                        "Inne działania realizujące cele Priorytetu II"
+                                    ]
+                                )
+                            ],
+                            components=[
+                                estimate_pt4.generate_estimate(),
+                            ]
+                        )
+                    ]
+                ),
+                estimate_pt1.generate_estimate_bottom()
+            ]
+        )
+
+        self.save_part(part=part)
---
+++ classes/form_builder/duk/dissemination/initiatives/estimate_data.py
@@ -0,0 +1,523 @@
+estimate_sections_pt1 = [
+    {
+        'title': '1. Koszty zarządzania przedsięwzięciem',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'costManagement'},
+            {'title': '- dyrektora festiwalu lub przeglądu', 'name': 'festivalDirector'},
+            {'title': '- dyrektora artystycznego/dyrektora programowego', 'name': 'artisticDirector'},
+            {'title': '- twórców, artystów', 'name': 'creatorsArtists'},
+            {'title': '- członków jury', 'name': 'juryMembers'},
+            {'title': '- osób prowadzących (np. imprezy towarzyszące, dyskusje panelowe, konferencje, spotkania z artystami, wystawy)', 'name': 'eventHosts'}
+        ]
+    },
+    {
+        'title': '2. Koszty konsultacji eksperckich, paneli eksperckich',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'expertConsultation'}
+        ]
+    },
+    {
+        'title': '3. Koszty tłumaczeń',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'translationCost'}
+        ]
+    },
+    {
+        'title': '4. Koszty obsługi PR',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'prServiceCosts'}
+        ]
+    },
+    {
+        'title': '5. Koszty promocji i reklamy',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'commercials'},
+            {'title': '- outdoor', 'name': 'outdoorCommercials'},
+            {'title': '- reklama prasowa, radiowa, telewizyjna, internetowa', 'name': 'mediaCommercials'}
+        ]
+    },
+    {
+        'title': '6. Koszty usług graficznych, fotograficznych i projektowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'graphicService'}
+        ]
+    },
+    {
+        'title': '7. Koszty usług poligraficznych/DTP',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'printingCosts'},
+            {'title': '- projekt', 'name': 'printingCostsProject'},
+            {'title': '- skład', 'name': 'printingCostsTypesetting'},
+            {'title': '- druk', 'name': 'printingCostsPrint'}
+        ]
+    },
+    {
+        'title': '8. Koszty obsługi finansowej',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'financialService'}
+        ]
+    },
+    {
+        'title': '9. Koszty związane z miejscem realizacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'locationCosts'},
+            {'title': '- koszty wynajmu sali lub powierzchni na potrzeby przedsięwzięcia', 'name': 'rentalCosts'},
+            {'title': '- koszty aranżacji przestrzeni, w tym zabezpieczenia BHP', 'name': 'spaceArrangementCosts'}
+        ]
+    },
+    {
+        'title': '10. Koszty obsługi przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'eventHostingCosts'},
+            {'title': '- koszty osobowe', 'name': 'personalCosts'},
+            {'title': '- koszty najmu sprzętu', 'name': 'equipmentRentalCosts'}
+        ]
+    },
+    {
+        'title': '11. Koszty usług transportowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'transportationService'}
+        ]
+    },
+    {
+        'title': '12. Koszty dotyczące podróży',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'tripCosts'},
+            {'title': '- organizatorów przedsięwzięcia', 'name': 'hostsTripCosts'},
+            {'title': '- twórców, artystów, prelegentów', 'name': 'participantsTripCosts'},
+            {'title': '- zaproszonych gości', 'name': 'guestsTripCosts'}
+        ]
+    },
+    {
+        'title': '13. Koszty dotyczące noclegów',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'accommodationCosts'},
+            {'title': '- organizatorów przedsięwzięcia', 'name': 'hostsAccommodationCosts'},
+            {'title': '- twórców, artystów, prelegentów', 'name': 'participantsAccommodationCosts'},
+            {'title': '- zaproszonych gości', 'name': 'guestsAccommodationCosts'}
+        ]
+    },
+    {
+        'title': '14. Koszty dokumentacji/rejestracji realizacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'documentationCosts'}
+        ]
+    },
+    {
+        'title': '15. Koszty ubezpieczeń',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'insuranceCosts'}
+        ]
+    },
+    {
+        'title': '16. Koszty praw autorskich',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'copyrightCosts'},
+            {'title': '- koszty nabycia praw autorskich lub licencji', 'name': 'copyrightsPurchaseCosts'},
+            {'title': '- koszty najmu kopii', 'name': 'copyRentalCosts'}
+        ]
+    },
+    {
+        'title': '17. Koszty nagród dla twórców, uczestniczących w konkursach',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'awardsCosts'}
+        ]
+    },
+    {
+        'title': '18. Koszty wydarzeń bezpośrednio związanych z realizacją przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'relatedEventsCosts'}
+        ]
+    },
+    {
+        'title': '19. Koszty cateringu',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'cateringCosts'}
+        ]
+    },
+    {
+        'title': '20. Koszt przeprowadzenia ewaluacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'evaluationCosts'}
+        ]
+    },
+    {
+        'title': '21. Koszty obsługi projektów online',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'onlineManagementCosts'}
+        ]
+    },
+    {
+        'title': '22. Koszty związane z przygotowaniem kopii filmowych oraz napisów do filmów',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'copyingAndSubtitlingCosts'}
+        ]
+    },
+    {
+        'title': '23. Koszty związane z dostosowaniem działań i formy przekazu do osób ze szczególnymi potrzebami, w tym osób z niepełnosprawnościami',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'accessibilityCosts'}
+        ]
+    }
+]
+
+estimate_sections_pt2 = [
+    {
+        'title': '1. Wypożyczenie materiałów wyjściowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'outputRentalCost'}
+        ]
+    },
+    {
+        'title': '2. Koszty digitalizacji',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'digitizationCost'}
+        ]
+    },
+    {
+        'title': '3. Koszty rekonstrukcji cyfrowej',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'restorationCost'}
+        ]
+    },
+    {
+        'title': '4. Koszty wykonania kopii wzorcowej oraz użytkowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'copyCreationCost'}
+        ]
+    },
+    {
+        'title': '5. Koszty zakupu dysków do przechowywania plików cyfrowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'storageDiskCost'}
+        ]
+    },
+    {
+        'title': '6. Koszty wykonania napisów',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'subtitlesCost'}
+        ]
+    },
+    {
+        'title': '7. Koszty wykonania audiodeskrypcji',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'audiodescriptionCost'}
+        ]
+    },
+    {
+        'title': '8. Koszty wykonania tłumaczenia PJM',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'pjmTranslationCost'}
+        ]
+    },
+    {
+        'title': '9. Koszty wykonania lektora',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'voiceoverCost'}
+        ]
+    },
+    {
+        'title': '10. Koszty korekty napisów / AD / PJM',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'correctionCost'}
+        ]
+    },
+    {
+        'title': '11. Koszty wykonania wersji rozpowszechniania',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'distributionVersionCost'}
+        ]
+    },
+    {
+        'title': '12. Koszty wykonania kopii wzorcowej wersji rozpowszechniania oraz kopii użytkowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'distributionMasterCost'}
+        ]
+    },
+    {
+        'title': '13. Koszty zakupu dysków do przechowywania wersji rozpowszechniania',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'distributionStorageDiskCost'}
+        ]
+    },
+    {
+        'title': '14. Koszty opracowania materiałów promocyjnych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'promoMaterialsCost'}
+        ]
+    },
+    {
+        'title': '15. Koszty promocji',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'promotionCost'}
+        ]
+    },
+    {
+        'title': '16. Koszty premiery/organizacji wydarzenia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'premiereCost'}
+        ]
+    },
+    {
+        'title': '17. Koszty koordynatora zadania',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'coordinatorCost'}
+        ]
+    },
+    {
+        'title': '18. Koszty prowadzenia dokumentacji zadania',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'documentationCost'}
+        ]
+    },
+    {
+        'title': '19. Koszty ewaluacji',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'evaluationCost'}
+        ]
+    }
+]
+
+estimate_sections_pt3 = [
+    {
+        'title': '1. Koszty przygotowania i zarządzania przedsięwzięciem',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'costManagement'},
+            {'title': '- twórców, artystów', 'name': 'creatorsArtists'},
+            {'title': '- prelegentów', 'name': 'speakers'},
+            {'title': '- osób prowadzących (np. spotkania z artystami)', 'name': 'eventHosts'},
+            {'title': '- koordynatorów (pokrywane wyłącznie ze środków Wnioskodawcy)', 'name': 'coordinators'}
+        ]
+    },
+    {
+        'title': '2. Koszty promocji i reklamy',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'commercials'}
+        ]
+    },
+    {
+        'title': '3. Koszty związane z miejscem realizacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'locationCosts'}
+        ]
+    },
+    {
+        'title': '4. Koszty obsługi przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'eventHostingCosts'},
+            {'title': '- koszty osobowe', 'name': 'personalCosts'},
+            {'title': '- najem sprzętu', 'name': 'equipmentRentalCosts'}
+        ]
+    },
+    {
+        'title': '5. Koszty dotyczące podróży',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'tripCosts'},
+            {'title': '- organizatorów przedsięwzięcia', 'name': 'hostsTripCosts'},
+            {'title': '- twórców, artystów, prelegentów', 'name': 'participantsTripCosts'},
+            {'title': '- zaproszonych gości', 'name': 'guestsTripCosts'}
+        ]
+    },
+    {
+        'title': '6. Koszty dotyczące noclegów',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'accommodationCosts'},
+            {'title': '- organizatorów przedsięwzięcia', 'name': 'hostsAccommodationCosts'},
+            {'title': '- twórców, artystów, prelegentów', 'name': 'participantsAccommodationCosts'},
+            {'title': '- zaproszonych gości', 'name': 'guestsAccommodationCosts'}
+        ]
+    },
+    {
+        'title': '7. Koszty składki Polskiej Federacji Dyskusyjnych Klubów Filmowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'polishDkfFederationFee'}
+        ]
+    },
+    {
+        'title': '8. Koszty nabycia praw autorskich lub licencji',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'copyrightCost'}
+        ]
+    },
+    {
+        'title': '9. Koszty najmu kopii filmowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'copyRentalCost'}
+        ]
+    },
+    {
+        'title': '10. Koszty obsługi projektów online',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'onlineManagementCost'}
+        ]
+    },
+    {
+        'title': '11. Koszty związane z dostosowaniem działań i formy przekazu do osób ze szczególnymi potrzebami, w tym osób z niepełnosprawnościami',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'accessibilityCost'}
+        ]
+    }
+]
+
+estimate_sections_pt4 = [
+    {
+        'title': '1. Koszty przygotowania i zarządzania przedsięwzięciem',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'costManagement'},
+            {'title': '- twórców, artystów', 'name': 'creatorsArtists'},
+            {'title': '- prelegentów, wykładowców', 'name': 'lecturers'},
+            {'title': '- komisarza wystawy', 'name': 'curator'},
+            {'title': '- koordynatorów przedsięwzięcia', 'name': 'organizers'},
+            {'title': '- osób prowadzących (np. imprezy towarzyszące, dyskusje panelowe, konferencje, spotkania z artystami, wystawy)', 'name': 'eventHosts'}
+        ]
+    },
+    {
+        'title': '2. Koszty tłumaczeń',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'translationCost'}
+        ]
+    },
+    {
+        'title': '3. Koszty promocji i reklamy',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'commercials'},
+            {'title': '- usługi PR', 'name': 'prServiceCosts'},
+            {'title': '- outdoor, reklamy prasowa, radiowa, telewizyjna, internetowa, standy, rollupy, banery', 'name': 'mediaCommercials'}
+        ]
+    },
+    {
+        'title': '4. Koszty przygotowania materiałów, redakcji i korekty',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'proofreadingCost'}
+        ]
+    },
+    {
+        'title': '5. Koszty usług graficznych, fotograficznych i projektowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'graphicService'}
+        ]
+    },
+    {
+        'title': '6. Koszty usług poligraficznych/DTP',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'printingCosts'},
+            {'title': '- projekt', 'name': 'printingCostsProject'},
+            {'title': '- skład', 'name': 'printingCostsTypesetting'},
+            {'title': '- druk', 'name': 'printingCostsPrint'}
+        ]
+    },
+    {
+        'title': '7. Koszty przygotowania materiałów multimedialnych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'multimediaMaterialsCost'}
+        ]
+    },
+    {
+        'title': '8. Koszty konsultacji eksperckich, paneli eksperckich',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'expertConsultation'}
+        ]
+    },
+    {
+        'title': '9. Koszt pozyskania praw autorskich',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'copyrightsCosts'}
+        ]
+    },
+    {
+        'title': '10. Koszt obsługi finansowej',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'financialService'}
+        ]
+    },
+    {
+        'title': '11. Koszty obsługi prawnej',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'lawAdvice'}
+        ]
+    },
+    {
+        'title': '12. Koszty związane z miejscem realizacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'locationCosts'},
+            {'title': '- koszty wynajmu miejsca oraz wyposażenia i obsługi sprzętu', 'name': 'rentalCosts'},
+            {'title': '- koszty aranżacji przestrzeni, w tym zabezpieczenia BHP', 'name': 'spaceArrangementCosts'}
+        ]
+    },
+    {
+        'title': '13. Koszty obsługi przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'eventHostingCosts'},
+            {'title': '- koszty osobowe', 'name': 'personalCosts'},
+            {'title': '- koszty najmu sprzętu', 'name': 'equipmentRentalCosts'}
+        ]
+    },
+    {
+        'title': '14. Koszty usług transportowych',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'transportationService'}
+        ]
+    },
+    {
+        'title': '15. Koszty dotyczące podróży',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'tripCosts'},
+            {'title': '- organizatorów przedsięwzięcia', 'name': 'hostsTripCosts'},
+            {'title': '- twórców, artystów, prelegentów', 'name': 'participantsTripCosts'},
+            {'title': '- zaproszonych gości', 'name': 'guestsTripCosts'}
+        ]
+    },
+    {
+        'title': '16. Koszty dotyczące noclegów',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'accommodationCosts'},
+            {'title': '- organizatorów przedsięwzięcia', 'name': 'hostsAccommodationCosts'},
+            {'title': '- twórców, artystów, prelegentów', 'name': 'participantsAccommodationCosts'},
+            {'title': '- zaproszonych gości', 'name': 'guestsAccommodationCosts'}
+        ]
+    },
+    {
+        'title': '17. Koszty niezbędnych ubezpieczeń',
+        'costs': [
+            {'isSum': True, 'title': 'Razem', 'name': 'insuranceAndSecurityCosts'},
+            {'title': '- ubezpieczenie', 'name': 'insuranceCosts'},
+            {'title': '- ochrona', 'name': 'securityCosts'}
+        ]
+    },
+    {
+        'title': '18. Koszty dokumentacji/rejestracji realizacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'documentationCosts'}
+        ]
+    },
+    {
+        'title': '19. Koszty nagród dla uczestników',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'awardsCosts'}
+        ]
+    },
+    {
+        'title': '20. Koszty cateringu',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'cateringCosts'}
+        ]
+    },
+    {
+        'title': '21. Koszty przeprowadzenia ewaluacji przedsięwzięcia',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'evaluationCosts'}
+        ]
+    },
+    {
+        'title': '22. Koszty obsługi projektów online',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'onlineManagementCosts'}
+        ]
+    },
+    {
+        'title': '23. Koszty związane z dostosowaniem działań i formy przekazu do osób ze szczególnymi potrzebami',
+        'costs': [
+            {'isSum': True, 'title': '', 'name': 'accessibilityCosts'}
+        ]
+    }
+]
