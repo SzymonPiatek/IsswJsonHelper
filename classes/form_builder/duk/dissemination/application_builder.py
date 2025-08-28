7f37d593362df707d1e9d43e0945c9b0db27d5e5
Szymon Piątek <szymon.piatek125@gmail.com>
2025-08-28 15:01:05 +0200

    Dissemination scope of the project - init

--- classes/form_builder/duk/dissemination/application_builder.py
+++ classes/form_builder/duk/dissemination/application_builder.py
@@ -10,3 +10,11 @@ class DisseminationApplicationBuilder(DUKApplicationBuilder):
         super().__init__()
 
         self.program_data_path = self.department_data_path / 'dissemination'
+        self.priority_data_path = None
+
+    def create_application_scope_of_project(self):
+        part = self.load_json(path=self.program_data_path / '_pages' / 'scope_of_the_project.json')
+        chapter = self.section.application_scope_of_project.cinema_project_relation_to_other_fundings()
+        chapter["title"] = "Czy przedsięwzięcie, na które składany jest wniosek jest powiązane z innymi przedsięwzięciami, o dofinansowanie których ubiega się Wnioskodawca w bieżącym roku z innych programów operacyjnych PISF? Jeżeli tak, proszę podać nazwę przedsięwzięcia, program oraz wnioskowaną kwotę dofinansowania"
+        part["chapters"].append(chapter)
+        self.save_part(part)
