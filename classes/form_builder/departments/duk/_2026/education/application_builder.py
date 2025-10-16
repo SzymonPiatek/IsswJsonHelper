91fd2f50ba8b01feabc961c95de2605e8a5001b6
Szymon Piątek <szymon.piatek125@gmail.com>
2025-10-16 09:56:46 +0200

    CP

--- classes/form_builder/departments/duk/_2026/education/application_builder.py
+++ classes/form_builder/departments/duk/_2026/education/application_builder.py
@@ -1,5 +1,4 @@
 from classes.form_builder.departments.duk._2026.application_builder import DUKApplicationBuilder2026
-from classes.helpers import int_to_roman
 
 
 class EducationApplicationBuilder(DUKApplicationBuilder2026):
