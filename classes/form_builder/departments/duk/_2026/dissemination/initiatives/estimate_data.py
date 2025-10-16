a763c28b3edc2ead5c833223844650687fba5002
Szymon Piątek <szymon.piatek125@gmail.com>
2025-10-16 13:40:43 +0200

    initiatives costs for dsks

--- classes/form_builder/departments/duk/_2026/dissemination/initiatives/estimate_data.py
+++ classes/form_builder/departments/duk/_2026/dissemination/initiatives/estimate_data.py
@@ -84,4 +84,226 @@ estimate_sections_pt124 = [
 ]
 
 estimate_sections_pt124 = [asdict(section) for section in estimate_sections_pt124]
-estimate_sections_pt3 = []
+
+estimate_sections_pt3 = [
+    EstimateSection(
+        title="Projekty filmowe realizowane przez SKS",
+        costs=[
+            CostItem(
+                title="Koszty honorariów dla artystów, prelegentów i prowadzących wydarzenia",
+                name="filmProjectsArtistFees",
+            ),
+            CostItem(
+                title="Koszty podróży i noclegów artystów, prelegentów i prowadzących wydarzenia",
+                name="filmProjectsTravelAndAccommodation",
+                helpText=(
+                    "Koszty udokumentowane wyłącznie fakturami lub biletami (jeśli niemożliwe jest otrzymanie faktury). "
+                    "W przypadku podróży lotniczych pokrywa się z dotacji tylko koszt biletów w klasie ekonomicznej."
+                ),
+            ),
+            CostItem(
+                title="Koszty licencji lub nabycia praw do publicznego pokazu i najmu kopii",
+                name="filmProjectsLicenseFees",
+            ),
+            CostItem(
+                title="Koszty przygotowania kopii filmowych i napisów do filmów",
+                name="filmProjectsFilmCopies",
+            ),
+            CostItem(
+                title="Koszty najmu sprzętu",
+                name="filmProjectsEquipmentRental",
+            ),
+            CostItem(
+                title="Koszty usług graficznych i poligraficznych",
+                name="filmProjectsGraphicServices",
+            ),
+            CostItem(
+                title="Koszty nagrań i usług fotograficznych",
+                name="filmProjectsRecordingServices",
+            ),
+            CostItem(
+                title="Koszty promocji i reklamy",
+                name="filmProjectsPromotionAdvertising",
+            ),
+            CostItem(
+                title="Koszty tłumaczeń",
+                name="filmProjectsTranslations",
+            ),
+            CostItem(
+                title="Koszty obsługi projektów online",
+                name="filmProjectsOnlineSupport",
+                helpText="Koszty dotyczące utrzymania platformy mojeekino.pl."
+            )
+        ],
+    ),
+
+    EstimateSection(
+        title="Wydarzenia branżowe realizowane przez SKS",
+        costs=[
+            CostItem(
+                title="Koszty honorariów dla prelegentów i prowadzących wydarzenia",
+                name="industryEventsSpeakerFees",
+            ),
+            CostItem(
+                title="Koszty podróży i noclegów prelegentów i prowadzących wydarzenia",
+                name="industryEventsTravelAndAccommodation",
+            ),
+            CostItem(
+                title="Koszty licencji lub nabycia praw do publicznego pokazu i najmu kopii",
+                name="industryEventsLicenseFees",
+            ),
+            CostItem(
+                title="Koszty przygotowania kopii filmowych i napisów do filmów",
+                name="industryEventsFilmCopies",
+            ),
+            CostItem(
+                title="Koszty najmu sprzętu",
+                name="industryEventsEquipmentRental",
+            ),
+            CostItem(
+                title="Koszty usług graficznych i poligraficznych",
+                name="industryEventsGraphicServices",
+            ),
+            CostItem(
+                title="Koszty nagrań i usług fotograficznych",
+                name="industryEventsRecordingServices",
+            ),
+            CostItem(
+                title="Koszty promocji i reklamy",
+                name="industryEventsPromotionAdvertising",
+            ),
+            CostItem(
+                title="Koszty tłumaczeń",
+                name="industryEventsTranslations",
+            ),
+            CostItem(
+                title="Koszty cateringu i poczęstunku",
+                name="industryEventsCatering",
+            ),
+            CostItem(
+                title="Koszty nagród branżowych",
+                name="industryEventsAwards",
+            ),
+        ],
+    ),
+
+    EstimateSection(
+        title="Wydarzenia realizowane przez kina studyjne",
+        costs=[
+            CostItem(
+                title="Koszty honorariów dla artystów, prelegentów i prowadzących wydarzenia",
+                name="cinemaEventsArtistFees",
+            ),
+            CostItem(
+                title="Koszty podróży i noclegów artystów, prelegentów i prowadzących wydarzenia",
+                name="cinemaEventsTravelAndAccommodation",
+                helpText=(
+                    "Koszty udokumentowane wyłącznie fakturami lub biletami (jeśli niemożliwe jest otrzymanie faktury). "
+                    "W przypadku podróży lotniczych pokrywa się z dotacji tylko koszt biletów w klasie ekonomicznej."
+                ),
+            ),
+            CostItem(
+                title="Koszty licencji lub nabycia praw do publicznego pokazu i najmu kopii",
+                name="cinemaEventsLicenseFees",
+            ),
+            CostItem(
+                title="Koszty przygotowania kopii filmowych i napisów do filmów",
+                name="cinemaEventsFilmCopies",
+            ),
+            CostItem(
+                title="Koszty najmu sprzętu",
+                name="cinemaEventsEquipmentRental",
+            ),
+            CostItem(
+                title="Koszty usług graficznych i poligraficznych",
+                name="cinemaEventsGraphicServices",
+            ),
+            CostItem(
+                title="Koszty nagrań i usług fotograficznych",
+                name="cinemaEventsRecordingServices",
+            ),
+            CostItem(
+                title="Koszty promocji i reklamy",
+                name="cinemaEventsPromotionAdvertising",
+            ),
+            CostItem(
+                title="Koszty tłumaczeń",
+                name="cinemaEventsTranslations",
+            ),
+        ],
+    ),
+
+    EstimateSection(
+        title="Koszty organizacji i obsługi SKS",
+        costs=[
+            CostItem(
+                title="Koszty zarządzania przedsięwzięciem",
+                name="organizationManagement",
+            ),
+            CostItem(
+                title="Koszty osobowe",
+                name="organizationPersonnelCosts",
+            ),
+            CostItem(
+                title="Koszty wynagrodzeń ekspertów",
+                name="organizationExpertFees",
+            ),
+            CostItem(
+                title="Koszty obsługi finansowej",
+                name="organizationFinancialServices",
+            ),
+            CostItem(
+                title="Koszty najmu biura",
+                name="organizationOfficeRental",
+            ),
+            CostItem(
+                title="Koszty promocji i reklamy",
+                name="organizationPromotionAdvertising",
+            ),
+            CostItem(
+                title="Koszty dotyczące podróży służbowych i noclegów",
+                name="organizationBusinessTravel",
+                helpText="Wyłącznie koszty dotyczące organizacji Konferencji Kin Studyjnych i Forum Edukacji Filmowej.",
+            ),
+            CostItem(
+                title="Działalność Rady Kin Studyjnych",
+                name="organizationActivitiesOfArthouseCinemaCouncil",
+                helpText="Pokrywane wyłącznie ze środków Wnioskodawcy."
+            )
+        ],
+    ),
+
+    EstimateSection(
+        title="Koszty związane z dostępnością",
+        costs=[
+            CostItem(
+                title="Dostępność cyfrowa (dostępne strony internetowe)",
+                name="accessibilityDigital",
+            ),
+            CostItem(
+                title="Dostępność informacyjno–komunikacyjna (wykonanie audiodeskrypcji, napisów SDH, tłumaczenia PJM)",
+                name="accessibilityInformationCommunication",
+            ),
+        ],
+    ),
+
+    EstimateSection(
+        title="Wsparcie i reprezentacja kin studyjnych",
+        costs=[
+            CostItem(
+                title="Szkolenia i warsztaty dla kin studyjnych",
+                name="supportWorkshops",
+            ),
+            CostItem(
+                title="Obsługa i reprezentacja prawna kin studyjnych",
+                name="supportLegalRepresentation",
+            ),
+            CostItem(
+                title="Rozpowszechnianie repertuaru studyjnego i dopłaty do seansów",
+                name="supportFilmDistribution",
+            ),
+        ],
+    ),
+]
+
+estimate_sections_pt3 = [asdict(section) for section in estimate_sections_pt3]
