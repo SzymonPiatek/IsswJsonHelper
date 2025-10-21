1b239dc4ed4203448c7439b1fd93fdb474d96ce5
Szymon Piątek <szymon.piatek125@gmail.com>
2025-10-21 12:54:50 +0200

    Fixes

--- classes/form_builder/departments/duk/_2026/dissemination/research/application_builder.py
+++ classes/form_builder/departments/duk/_2026/dissemination/research/application_builder.py
@@ -11,12 +11,10 @@ class ResearchApplicationBuilder(DisseminationApplicationBuilder, ResearchPriori
         super().__init__()
 
         self.project_type = [
-            "Badania ilościowe i jakościowe o charakterze cyklicznym, dotyczące widza kinowego oraz bilansu kompetencji.",
-            "Badania rynkowe w sferze kinematografii.",
-            "Przygotowanie analiz w zakresie organizacji i finansowania rynku kinematograficznego w Polsce, a także przedsięwzięć wspierających jego systemowy rozwój.",
+            "Badania widowni kinowej oraz bilansu kompetencji (ilościowe, jakościowe, cykliczne).",
+            "Badania i analizy rynku kinematograficznego.",
             "Przygotowanie innowacyjnych przedsięwzięć o szczególnym znaczeniu dla rozwoju rynku audiowizualnego.",
-            "Badania i analizy zjawiska piractwa w sferze kinematografii oraz wdrażanie przedsięwzięć, których celem jest zwalczanie piractwa i zapobieganie łamaniu praw autorskich.",
-            "Inne działania realizujące cele Priorytetu V."
+            "Badania i analizy zjawiska tzw. piractwa w sferze kinematografii oraz działania na rzecz jego zwalczania."
         ]
 
         estimate_builder = DUKApplicationEstimateBuilder(estimate_sections=estimate_sections)
@@ -25,3 +23,113 @@ class ResearchApplicationBuilder(DisseminationApplicationBuilder, ResearchPriori
         ]
 
         self.source_of_financing_tickets = True
+
+    def create_application_scope_of_project(self, number):
+        part = self.create_part(
+            title=f"{self.helpers.int_to_roman(number)}. Zakres przedsięwzięcia i jego charakterystyka",
+            short_name=f"{self.helpers.int_to_roman(number)}. Zakres przedsięwziecia",
+            chapters=[
+                self.create_chapter(
+                    title="1. Zakres przedsięwzięcia i jego charakterystyka",
+                    components=[
+                        self.create_chapter(
+                            title="Zakres przedmiotowo-tematyczny badania",
+                            help_text="Merytoryczny zakres badania, główne tematy i obszary analizy.",
+                            components=[
+                                self.create_component(
+                                    name="subjectAndTopicScopeOfStudy",
+                                    component_type="textarea",
+                                    validators=[
+                                        self.validator.length_validator(
+                                            max_value=3000
+                                        )
+                                    ],
+                                    required=True,
+                                )
+                            ]
+                        ),
+                        self.create_chapter(
+                            title="Analiza zapotrzebowania rynku na realizację przedsięwzięcia",
+                            help_text="Identyfikacja potrzeb i oczekiwań oraz zastosowanie praktyczne rezultatów badania.",
+                            components=[
+                                self.create_component(
+                                    name="marketAnalysisForProjectImplementation",
+                                    component_type="textarea",
+                                    validators=[
+                                        self.validator.length_validator(
+                                            max_value=3000
+                                        )
+                                    ],
+                                    required=True,
+                                )
+                            ]
+                        ),
+                        self.create_chapter(
+                            title="Metody realizacji i narzędzie technologiczne zastosowane w projekcie",
+                            help_text="Organizacja, przebieg i zaplecze technologiczne przedsięwzięcia, zastosowanie innowacyjnych metod badawczych.",
+                            components=[
+                                self.create_component(
+                                    name="implementationMethodsAndTechnologicalToolsUsedInProject",
+                                    component_type="textarea",
+                                    validators=[
+                                        self.validator.length_validator(
+                                            max_value=3000
+                                        )
+                                    ],
+                                    required=True,
+                                )
+                            ]
+                        ),
+                        self.create_chapter(
+                            title="Grupa docelowa realizowanego przedsięwzięcia",
+                            help_text="Adresaci i praktyczni użytkownicy efektów badawczych.",
+                            components=[
+                                self.create_component(
+                                    name="targetGroup",
+                                    component_type="textarea",
+                                    validators=[
+                                        self.validator.length_validator(
+                                            max_value=3000
+                                        )
+                                    ],
+                                    required=True,
+                                )
+                            ]
+                        ),
+                        self.create_chapter(
+                            title="Doświadczenie wnioskodawcy i kompetencje zespołu",
+                            help_text="W tym udział specjalistów w realizację przedsięwzięcia.",
+                            components=[
+                                self.create_component(
+                                    component_type="textarea",
+                                    name="applicantAndTeamExperience",
+                                    validators=[
+                                        self.validator.length_validator(
+                                            max_value=3000
+                                        )
+                                    ],
+                                    required=True
+                                ),
+                            ]
+                        ),
+                        self.create_chapter(
+                            title="Planowane efekty realizacji przedsięwzięcia i sposób ich prezentacji ",
+                            help_text="Oczekiwane rezultaty badania, forma publikacji wyników oraz sposób ich rozpowszechnienia.",
+                            components=[
+                                self.create_component(
+                                    component_type="textarea",
+                                    name="plannedEffects",
+                                    validators=[
+                                        self.validator.length_validator(
+                                            max_value=3000
+                                        )
+                                    ],
+                                    required=True
+                                ),
+                            ]
+                        ),
+                    ]
+                )
+            ]
+        )
+        self.save_part(part)
